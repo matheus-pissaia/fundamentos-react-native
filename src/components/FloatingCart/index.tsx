b619f43dd7b9b561a02ae6c86e0cd8f184a7b77c
Matheus Pissaia <pissaia.matheus03@gmail.com>
2020-10-29 11:30:07 -0300

    :sparkles: Desafio Fundamentos React-Native Finalizado

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -23,16 +23,30 @@ const FloatingCart: React.FC = () => {
 
   const navigation = useNavigation();
 
+  /**
+   * Funçao responsável por calcular o PREÇO TOTAL do carrinho
+   */
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const subtotalPrice = product.quantity * product.price;
 
-    return formatValue(0);
+      return accumulator + subtotalPrice;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
+  /**
+   * Funçao responsável por calcular a QUANTIDADE TOTAL de itens no carrinho
+   */
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const subtotalQuantity = product.quantity;
 
-    return 0;
+      return accumulator + subtotalQuantity;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -29,24 +29,88 @@ const CartProvider: React.FC = ({ children }) => {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
+    /**
+     * Carregar produtos armazenados no Async Storage
+     */
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const productsInStorage = await AsyncStorage.getItem(
+        'GoMarketplace:products',
+      );
+
+      if (productsInStorage) {
+        setProducts([...JSON.parse(productsInStorage)]);
+      }
     }
 
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+  /**
+   * Funçao responsável por adicionar um produto no carrinho, verficando se ele já existe no carrinho ou nao
+   */
+  const addToCart = useCallback(
+    async product => {
+      const productExists = products.find(item => item.id === product.id);
 
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      if (productExists) {
+        setProducts(
+          products.map(item =>
+            item.id === product.id
+              ? { ...product, quantity: item.quantity + 1 }
+              : item,
+          ),
+        );
+      } else {
+        setProducts([...products, { ...product, quantity: 1 }]);
+      }
 
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      await AsyncStorage.setItem(
+        'GoMarketplace:products',
+        JSON.stringify(products),
+      );
+    },
+    [products],
+  );
+
+  /**
+   * Funçao responsável pelo AUMENTO da QUANTIDADE de um produto no carrinho
+   */
+  const increment = useCallback(
+    async id => {
+      const productIncremented = products.map(product =>
+        product.id === id
+          ? { ...product, quantity: product.quantity + 1 }
+          : product,
+      );
+      setProducts(productIncremented);
+
+      await AsyncStorage.setItem(
+        'GoMarketplace:products',
+        JSON.stringify(productIncremented),
+      );
+    },
+    [products],
+  );
+
+  /**
+   * Funçao responsável pelo DECRÉSCIMO da QUANTIDADE de um produto no carrinho
+   */
+  const decrement = useCallback(
+    async id => {
+      const productDecremented = products.map(product =>
+        product.id === id
+          ? { ...product, quantity: product.quantity - 1 }
+          : product,
+      );
+      setProducts(productDecremented);
+
+      await AsyncStorage.setItem(
+        'GoMarketplace:products',
+        JSON.stringify(productDecremented),
+      );
+    },
+    [products],
+  );
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
